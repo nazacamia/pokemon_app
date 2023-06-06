a7906f9a0f1aa75a69d5425365070d8d03e4daea
nazareno <n.camia@campus.unimib.it>
2023-06-06 22:15:40 +0200

    - aggiunta sezione preferiti

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -1,7 +1,9 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:pokemon_app/logic/pokemon_list_controller.dart';
 import 'package:pokemon_app/model/pokemon.dart';
+import 'package:pokemon_app/presentation/screens/preferred_screen.dart';
 import 'package:pokemon_app/presentation/widgets/pokemon_tile.dart';
 
 class HomePage extends StatefulWidget {
@@ -30,13 +32,20 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         centerTitle: false,
         backgroundColor: Colors.red,
+        actions: [
+          IconButton(
+              onPressed: (){
+                Navigator.of(context).push(MaterialPageRoute(builder: (context) => const PreferredScreen()));
+              },
+              icon: const Icon(CupertinoIcons.heart_fill, color: Colors.black,))
+        ],
         title: Text(widget.title),
         bottom: const PreferredSize(
           preferredSize: Size.fromHeight(60),
           child: Stack(
             alignment: Alignment.center,
             children: [
-              SizedBox(height: 60,),
+              SizedBox(height: 60),
             ],
           ),
         ),
@@ -61,6 +70,7 @@ class _HomePageState extends State<HomePage> {
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
             FloatingActionButton(
+              heroTag: 'btn1',
               onPressed: (_pageIndex <= 1)
                   ? null
                   : () {
@@ -73,6 +83,7 @@ class _HomePageState extends State<HomePage> {
               child: const Icon(Icons.arrow_back_ios_sharp),
             ),
             FloatingActionButton(
+              heroTag: 'btn2',
               onPressed: () {
                 setState(() {
                   _pageIndex++;
---
+++ lib/presentation/screens/preferred_screen.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:pokemon_app/logic/pokemon_list_controller.dart';
+import 'package:pokemon_app/presentation/widgets/pokemon_tile.dart';
+
+class PreferredScreen extends StatelessWidget {
+  const PreferredScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: const Text('Pokedex - preferiti'),
+
+      ),
+      body: Obx(() {
+        if (PokemonListController.to.isLoading.value) {
+          return const Center(child: CircularProgressIndicator());
+        } else {
+          return ListView.builder(
+              itemCount: PokemonListController.to.preferredList.length,
+              itemBuilder: (context, index) {
+                return PokemonTile(
+                  pokemon: PokemonListController.to.preferredList[index],
+                  index: index + 1,
+                );
+              });
+        }
+      }),
+
+    );
+  }
+}
