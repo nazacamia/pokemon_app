7aedb4c92fe5ec81c1173a93fc6d3e5fe01ebcfc
nazareno <n.camia@campus.unimib.it>
2023-06-06 20:34:05 +0200

    - Aggiornato UI - Aggiunto pagine successive

--- lib/model/pokemon.dart
+++ lib/model/pokemon.dart
@@ -1,8 +1,10 @@
+import 'package:flutter/material.dart';
+
 class Pokemon {
   late final String name;
   late final String url;
-
-  Pokemon({required this.name, required this.url});
+  late final Color color;
+  Pokemon({required this.name, required this.url, required this.color});
 
   @override
   String toString(){
--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -1,12 +1,11 @@
 
-import 'dart:convert';
+import 'dart:async';
 
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:pokemon_app/logic/pokemon_list_controller.dart';
 import 'package:pokemon_app/model/pokemon.dart';
-import 'package:http/http.dart' as http;
+import 'package:palette_generator/palette_generator.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key, required this.title});
@@ -18,16 +17,16 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  int _pageIndex = 0;
+  int _pageIndex = 1;
   List<dynamic> rawPokemonList = [];
   List<Pokemon> pokemonList = [];
-
+  int offset= 0;
   @override
   void initState() {
     super.initState();
     PokemonListController.to.fetchPokemon(_pageIndex);
-  }
 
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -40,7 +39,7 @@ class _HomePageState extends State<HomePage> {
         child: Obx(
             (){
               if (PokemonListController.to.isLoading.value){
-                return CircularProgressIndicator();
+                return const CircularProgressIndicator();
               }
               else {
                 print('notloading');
@@ -48,27 +47,109 @@ class _HomePageState extends State<HomePage> {
           return ListView.builder(
               itemCount: PokemonListController.to.pokemonList.length,
               itemBuilder: (context, index) {
-                return MaterialButton(
-                  onPressed: () {},
-                  child: Row(
-                    children: [
-                      Image.network(PokemonListController.to.pokemonList[index].url),
-                      Text(PokemonListController.to.pokemonList[index].name),
-                    ],
-                  ),
-                );
+                return PokemonTile(pokemon: PokemonListController.to.pokemonList[index], index: index + offset + 1,);
               });
         }
       }
         )
       ),
-      bottomNavigationBar: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          FloatingActionButton(onPressed: () {  },),
-          FloatingActionButton(onPressed: () {  },),
-        ],
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            FloatingActionButton(
+              onPressed:  (_pageIndex<=1)? null: () {
+                setState(() {
+                  _pageIndex--;
+                  offset=offset-50;
+                });
+                PokemonListController.to.fetchPokemon(_pageIndex);
+                },
+              child: const Icon(Icons.arrow_back_ios_sharp),
+            ),
+            FloatingActionButton(
+              onPressed: () {
+                setState(() {
+                  _pageIndex++;
+                  offset=offset+50;
+                });
+                PokemonListController.to.fetchPokemon(_pageIndex);
+              },
+              child: const Icon(Icons.arrow_forward_ios_sharp),
+
+            ),
+          ],
+        ),
       ),
     );
   }
+}
+
+class PokemonTile extends StatefulWidget {
+  const PokemonTile({Key? key, required this.pokemon, required this.index}) : super(key: key);
+  final Pokemon pokemon;
+  final int index;
+  @override
+  State<PokemonTile> createState() => _PokemonTileState();
+}
+
+class _PokemonTileState extends State<PokemonTile> {
+  late final Color color = widget.pokemon.color;
+  @override
+  void initState()  {
+    super.initState();
+    //getCardColour();
+  }
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    if (widget.pokemon.name == 'ledian' || widget.pokemon.name=='spinarak'|| widget.pokemon.name=='ariados') {
+      print('widget.pokemon.color');
+      print(widget.pokemon.color);
+      //0xff181818
+    }
+    if (widget.pokemon.color == Color(0xff181818)) {
+      print('nero');
+    }
+
+    return Card(
+      color: color == Color(0xff181818) || color == Color(0xff101010)? Colors.blueGrey:color,
+      child: MaterialButton(
+        padding: const EdgeInsets.all(10),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4.0)),
+        onPressed: () {},
+        child: Column(
+          children: [
+            Stack(
+              alignment: Alignment.center,
+              children: [
+                Container(
+                  height: 100,
+                  width: 100,
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    shape: BoxShape.circle
+                  ),
+                ),
+                Container(
+                  height: 8,
+                  width: 100,
+                  color: widget.pokemon.color,
+                ),
+                Image.network(widget.pokemon.url)
+              ],
+            ),
+            const SizedBox(height: 10,),
+            Text('#${widget.index} ', style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20),),
+            Text(widget.pokemon.name[0].toUpperCase() + widget.pokemon.name.substring(1), style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 18)),
+          ],
+        ),
+      ),
+    );
+  }
+
+
 }
